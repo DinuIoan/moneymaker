2c30fe4f2d5b4886650f488e779dbcd463209e84
DinuIoan <dinu.ioan21@gmail.com>
2018-09-22 16:20:27 +0300

    handling fragments

--- app/src/main/java/com/bestapps/moneymaker/MainActivity.java
+++ app/src/main/java/com/bestapps/moneymaker/MainActivity.java
@@ -1,9 +1,10 @@
 package com.bestapps.moneymaker;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,10 +15,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.bestapps.moneymaker.earnmoney.EarnMoney;
+import com.bestapps.moneymaker.earnmoney.EarnMoneyFragment;
+import com.bestapps.moneymaker.home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        fragmentManager = getSupportFragmentManager();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,27 +87,20 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_earn_money) {
-            Intent intent = new Intent(getApplicationContext(), EarnMoney.class);
-            startActivity(intent);
-            finish();
+           FragmentTransaction fragmentTransaction =
+                    fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_placeholder, new EarnMoneyFragment());
+            fragmentTransaction.commit();
+        }
+        if (id == R.id.nav_home) {
+            FragmentTransaction fragmentTransaction =
+                    fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_placeholder, new HomeFragment());
+            fragmentTransaction.commit();
         }
         if (id == R.id.nav_exit) {
             quitApp();
         }
-
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ app/src/main/java/com/bestapps/moneymaker/earnmoney/EarnMoneyFragment.java
@@ -0,0 +1,25 @@
+package com.bestapps.moneymaker.earnmoney;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bestapps.moneymaker.R;
+
+public class EarnMoneyFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_earn_money_layout, container, false);
+    }
+}
